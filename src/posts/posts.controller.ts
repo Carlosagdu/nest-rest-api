1d18ddbe613535a56bd02ab545aceec3301b589f
Carlos Aguilar Duarte <car2275580@maricopa.edu>
2021-08-01 22:33:44 -0500

    AWS S3 API implementation

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,6 +10,7 @@ import {
   UploadedFile,
   Res,
 } from '@nestjs/common';
+var fs = require('fs');
 import { FileInterceptor } from '@nestjs/platform-express';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -18,7 +19,12 @@ import { Express } from 'express';
 import { diskStorage } from 'multer';
 import { editFileName, imageFileFilter } from './utils/posts.util';
 import { CreateCommentDto } from './dto/create-comment.dto';
+var AWS = require('aws-sdk');
 
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS,
+});
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
@@ -42,6 +48,21 @@ export class PostsController {
     }),
   )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
+    fs.readFile(file.path, (err, data) => {
+      if (err) throw err;
+      const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: file.filename,
+        Body: data,
+      };
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.log('there was an error', err);
+        } else {
+          console.log('successfully uploaded data');
+        }
+      });
+    });
     const response = {
       originalname: file.originalname,
       filename: file.filename,
